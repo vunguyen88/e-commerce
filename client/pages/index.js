6516ed4a4a2b88f4ba5745abba66e9e1eba1f9d4
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2022-04-07 17:46:28 -0400

    redeploy cluster

--- client/pages/index.js
+++ client/pages/index.js
@@ -93,7 +93,7 @@ const Landing =  () => {
 };
 
 Landing.getInitialProps = async (context, client, currentUser) => {
-    console.log('test');
+    console.log('get initialprops in client');
     try {
         const { data } = await client.get('/api/products');
         return { products: data };
