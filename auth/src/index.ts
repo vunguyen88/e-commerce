ab5e04c747481bdcb6b795448073cd5a25419312
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2022-04-11 17:30:43 -0400

    restart all pods

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-    console.log('Starting auth service...'); 
-    console.log('mongo env ', process.env.MONGO_URI);
+    console.log('Starting auth service....'); 
     
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
